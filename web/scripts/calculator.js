a3d11456cc87628ca16ba3feaad7f0ba95c8d859
RobertoRojas <10171647+RobertoRojas@users.noreply.github.com>
2026-03-01 22:56:29 -0600

    Changing order of items

--- web/scripts/calculator.js
+++ web/scripts/calculator.js
@@ -14,36 +14,15 @@ function getFromAmount(item, amount, totals = {}) {
     return totals;
 }
 
-function getMaxCraftable(target, inventory) {
-    const perUnit = getFromAmount(target, 1, {});
-
-    let max = Infinity;
-
-    for (let item in perUnit) {
-        if (item === target) continue;
-
-        const have = inventory[item] || 0;
-        const need = perUnit[item];
-
-        max = Math.min(max, Math.floor(have / need));
+function getBasicRequirements(item, amount, totals = {}) {
+    if (!RECIPES[item] || Object.keys(RECIPES[item]).length === 0) {
+        totals[item] = (totals[item] || 0) + amount;
+        return totals;
+    }
+    for (const ingredient in RECIPES[item]) {
+        const needed = RECIPES[item][ingredient] * amount;
+        getBasicRequirements(ingredient, needed, totals);
     }
 
-    if (max === Infinity) max = 0;
-
-    const missing = {};
-    for (let item in perUnit) {
-        if (item === target) continue;
-
-        const required = perUnit[item] * (max + 1);
-        const have = inventory[item] || 0;
-
-        if (have < required) {
-            missing[item] = required - have;
-        }
-    }
-
-    return {
-        maxCraftable: max + 1,
-        missingForNext: missing
-    };
+    return totals;
 }
